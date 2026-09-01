d2ce5ff849559f83a40beb83630aa637e2e560bc
Karan Sardar <snsardarkaran61@gmail.com>
2026-09-01 19:46:52 +0530

    initial commit

---
+++ backend-part/src/main/java/com/vulprioritizer/backend_part/BackendPartApplication.java
@@ -0,0 +1,13 @@
+package com.vulprioritizer.backend_part;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPartApplication.class, args);
+	}
+
+}
---
+++ backend-part/src/test/java/com/vulprioritizer/backend_part/BackendPartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vulprioritizer.backend_part;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
