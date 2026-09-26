8fe4394f2f300b37ed2f862ed74eabd26a9c4d4f
Karan Sardar <snsardarkaran61@gmail.com>
2026-09-26 11:30:27 +0530

    fixed bug

--- backend-part/src/main/java/com/vulprioritizer/backend_part/auth/repository/ForgotPasswordResetTokenRepository.java
+++ backend-part/src/main/java/com/vulprioritizer/backend_part/auth/repository/ForgotPasswordResetTokenRepository.java
@@ -3,11 +3,16 @@ package com.vulprioritizer.backend_part.auth.repository;
 import com.vulprioritizer.backend_part.auth.entity.ForgotPasswordResetToken;
 import com.vulprioritizer.backend_part.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ForgotPasswordResetTokenRepository extends JpaRepository<ForgotPasswordResetToken, Long> {
     Optional<ForgotPasswordResetToken> findByToken(String token);
 
-    void deleteByUser(User user);
+    @Modifying
+    @Query("DELETE FROM ForgotPasswordResetToken t WHERE t.user = :user")
+    void deleteByUser(@Param("user") User user);
 }
